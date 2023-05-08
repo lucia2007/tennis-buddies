8540508063c88a10155b521ee1b17c3c7167163c
lucia2007 <>
2023-05-08 15:38:48 +0000

    bug-fix deployment

--- tennisbuddies/settings.py
+++ tennisbuddies/settings.py
@@ -20,8 +20,7 @@ if os.path.isfile("env.py"):
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-TEMPLATES_DIR = os.path.join(BASE_DIR,
-                             'templates')
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -33,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['tennisbuddies.herokuapp.com', 'localhost',
+ALLOWED_HOSTS = ['tennis-buddies.herokuapp.com', 'localhost',
                  '8000-lucia2007-tennis-buddies-rhs76777q5.us2.codeanyapp.com']
 
 
@@ -136,16 +135,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_STORAGE =
-'cloudinary_storage.storage.StaticHashedCloudin
-aryStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR,
-                                 'static'), ]
-STATIC_ROOT = os.path.join(BASE_DIR,
-                           'staticfiles')
+'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_URL = '/media/'
-DEFAULT_FILE_STORAGE =
-'cloudinary_storage.storage.MediaCloudinaryStor
-age'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
